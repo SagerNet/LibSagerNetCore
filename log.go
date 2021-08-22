de5e91693cbec990e93c76c8caeadf2f0336e81d
世界 <sekai@neko.services>
2021-08-22 23:36:58 +0800

    Fix log

--- log.go
+++ log.go
@@ -14,6 +14,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"log"
 	"os"
+	"strings"
 	"syscall"
 	"unsafe"
 
@@ -41,7 +42,8 @@ type androidHook struct {
 type androidFormatter struct{}
 
 func (f *androidFormatter) Format(entry *logrus.Entry) ([]byte, error) {
-	msgWithLevel := fmt.Sprint("[", entry.Level.String(), "] ", entry.Message)
+
+	msgWithLevel := fmt.Sprint("[", strings.Title(entry.Level.String()), "] ", entry.Message)
 	return []byte(msgWithLevel), nil
 }
 
