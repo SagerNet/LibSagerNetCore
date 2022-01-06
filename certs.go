fb0ec2d6e2b7a92214c8b1c3450f71f1537baa5d
世界 <i@sekai.icu>
2022-01-06 13:14:17 +0800

    Add hook to replace x509/systemRoots

---
+++ certs.go
@@ -0,0 +1,46 @@
+package libcore
+
+import (
+	"crypto/x509"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+	_ "unsafe" // for go:linkname
+)
+
+//go:linkname systemRoots crypto/x509.systemRoots
+var systemRoots *x509.CertPool
+
+func updateRootCACerts() {
+	x509.SystemCertPool()
+	roots := x509.NewCertPool()
+	pemFile, err := ioutil.ReadFile(internalAssetsPath + mozillaIncludedPem)
+	if err != nil {
+		logrus.Warn("failed to load root ca certificates from internal assets dir: ", err)
+		return
+	}
+	if !roots.AppendCertsFromPEM(pemFile) {
+		logrus.Warn("failed to append certificates from pem")
+		return
+	}
+	systemRoots = roots
+	logrus.Info("updated root ca certificate list")
+}
+
+//go:linkname initSystemRoots crypto/x509.initSystemRoots
+func initSystemRoots()
+
+var disableSystem bool
+
+func UpdateSystemRoots(useSystem bool) {
+	if disableSystem != useSystem {
+		return
+	}
+	disableSystem = !disableSystem
+
+	if useSystem {
+		initSystemRoots()
+		logrus.Info("reset systemRoots")
+	} else {
+		updateRootCACerts()
+	}
+}
